bfa065f4245a65257c6a25c9d0fe82601800e0cd
shallweeee <phcphc82@gmail.com>
2021-01-28 22:36:11 +0900

    Update dash

---
+++ README.md
@@ -0,0 +1,30 @@
+Dash 설치
+```
+conda create -y -n dash python=3.8.5
+conda activate dash
+pip install -r requirements.txt
+```
+
+Dash 실행
+
+- 터미널
+```
+(dash) $ python app.py
+```
+- VSCODE
+```
+F5
+또는
+Ctrl + Shft + D > Python: Dash > Start Debugging
+```
+
+
+[VSCODE 단축키](https://demun.github.io/vscode-tutorial/shortcuts/)
+
+|단축키|기능
+|---|---
+|`Ctrl + K V`|마크다운 프리뷰
+|``Ctrl + ` ``|터미널창 토글
+|`Ctrl + /`| 라인 주석 토글
+|`Ctrl + Shift + P`| 모든 명령
+|`Tab, Shft + Tab`| 인덴트
--- app.py
+++ app.py
@@ -1,35 +1,28 @@
-#! /usr/bin/env python3
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
-import pandas as pd
+from dash.dependencies import Input, Output
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+application = app.server  # noqa
 
-df = pd.DataFrame({
-    'Fruit': ['Apples', 'Oranges', 'Bananas', 'Apples', 'Oranges', 'Bananas'],
-    'Amount': [4, 1, 2, 2, 4, 5],
-    'City': ['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal']
-})
-
-fig = px.bar(df, x='Fruit', y='Amount', color='City', barmode='group')
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+app.layout = html.Div([
+    html.Div(["Name: ", dcc.Input(id="my-input", type="text", debounce=True)]),
+    html.Br(),
+    html.Div(id="my-output"),
 ])
 
-if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', debug=True)
+
+@app.callback(
+    Output("my-output", "children"),
+    Input("my-input", "value"),
+    prevent_initial_call=True,
+)
+def update_output_div(name):
+    return f"Hello {name} !"
+
+
+if __name__ == "__main__":
+    app.run_server(host="0.0.0.0", port=8050, debug=True)
